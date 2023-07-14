2ebaa1b51d9d13371524910ceb9436e9e0012488
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-07-14 16:37:37 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/*
+* main func*/ int main(void)
+{
+int i;
+
+/* putchar loop func */
+for (i = 97; i < 123; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
