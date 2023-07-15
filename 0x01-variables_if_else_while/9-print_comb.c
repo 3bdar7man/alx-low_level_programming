a8779a966920273ca45dd91d6a4a945e04bb146b
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-07-14 18:31:54 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+/* betty style doc for function main goes there */
+/*
+*main func */ int main(void)
+{
+/* i var loop */
+int i;
+
+/* putchar loop func */
+for (i = 0; i > 10; i++)
+{
+/* putchar digit func */
+putchar(i);
+if (i != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
