53ad1a7a5dcc47f99f283fe7aa01bbb6e4e151fa
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-07-14 18:31:49 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/*
+*main func - short description */ int main(void)
+{
+/* i var loop */
+int i;
+
+/* putchar loop func */
+for (i = 97; i < 123; i++)
+{
+/* if condition func */
+if (i != 101 && i != 123)
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
