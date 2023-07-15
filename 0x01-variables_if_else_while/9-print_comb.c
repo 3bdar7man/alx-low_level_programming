6208ab39f0dc2e59152c80408e7fc1357c36cdc2
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-07-15 08:37:58 -0700

    9-print_comb.c (-repeat-)

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
-/*
-*main func */ int main(void)
+/**
+ * main - excuted program
+ * Return: success, the program executed successfully
+ */
+int main(void)
 {
-/* i var loop */
 int i;
 
-/* putchar loop func */
 for (i = 0; i < 10; i++)
 {
 /* putchar digit func */
-putchar(i % 10 + '0');
+putchar(i);
 if (i != 9)
 {
 putchar(',');
