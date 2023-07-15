d166117413b4419625ec6be388f869194478a528
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-07-14 18:31:52 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/*
+*main func - short description */ int main(void)
+{
+/* i var loop */
+int i;
+
+/* putchar loop func */
+for (i = 122; i > 96; i--)
+putchar(i);
+putchar('\n');
+return (0);
+}
