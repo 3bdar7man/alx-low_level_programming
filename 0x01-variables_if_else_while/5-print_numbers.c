45aaf735b598ee4e5c752a28514c2d0b3cb59008
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-07-14 18:31:50 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/*
+*main func */ int main(void)
+{
+/* i var loop */
+int i;
+
+/* putchar loop func */
+for (i = 0; i < 10; i++)
+printf("%d", i);
+printf("\n");
+return (0);
+}
