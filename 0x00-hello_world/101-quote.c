68adfeb58c43b8ac9ef66a6121214258c2b3108c
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-07-17 05:47:15 -0700

    101-quote.c (-repeat-)

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include <string.h>
+
+/**
+ * main - excuted program
+ * Return: Description of the returned value
+ */
 int main(void)
 {
+fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof(char), strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"), stdout);
 return (1);
 }
