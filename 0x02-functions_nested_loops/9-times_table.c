f370afa56d79273298aaad04d393eeefc615e5de
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-07-19 19:57:16 -0700

    addition commit for 9-

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,5 +5,15 @@
  */
 void times_table(void)
 {
-printf("0,  0,  0,  0,  0,  0,  0,  0,  0,  0\n");
+int i, j;
+
+for (i = 0; i < 10; i++)
+{
+printf("%d", 0);
+for (j = 1; j < 10; j++)
+{
+(j * i) < 10 ? printf(",  %d", j * i) : printf(", %d", j * i);
+}
+printf("\n");
+}
 }
