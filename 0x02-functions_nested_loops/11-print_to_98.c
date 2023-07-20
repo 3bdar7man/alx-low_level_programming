80db5610f1af140dc876243dfdb9db259dff9f02
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-07-19 20:20:32 -0700

    addition commit for 11-

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,10 +7,24 @@
 void print_to_98(int n)
 {
 int i;
+
 printf("%d", n);
-if (n != 98)
-for (i = n; n <= 98; i++) 
+if (n > 98)
+goto nGreater;
+else if (n < 98)
+goto nLess;
+printf("\n");
+nLess:
+for (i = n + 1; n <= 98; i++)
+{
+printf", %d", i);
+}
+printf("\n");
+return;
+nGreater:
+for (i = n - 1; n >= 98; i--) 
 {
 printf(", %d", i);
 }
+printf("\n");
 }
