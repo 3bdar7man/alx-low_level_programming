9dbba65a77f0756150fb87bff5982a72ae96df87
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-07-19 19:01:36 -0700

    addition commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - print some thing
+ * @n: some thing parameter
+ */
+void print_to_98(int n)
+{
+int i;
+printf("%d", n);
+if (n != 98)
+for (i = n; n <= 98; i++) 
+{
+printf(", %d", i)
+}
+}
