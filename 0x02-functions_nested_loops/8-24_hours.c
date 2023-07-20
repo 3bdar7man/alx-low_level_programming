ff2f5c82db18897d9dde79d541c52aa25ec3412c
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-07-19 19:27:37 -0700

    addition commit for 8-

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,5 +5,16 @@
  */
 void jack_bauer(void)
 {
-printf("00:00\n23:59");
+int i, j;
+char* iStr, jStr;
+
+for (i = 0; i < 24; i++)
+{
+i < 10 ? fprintf(iStr, "0%d", i) : fprintf(iStr, "%d", i);
+for (j = 0; j < 60; j++)
+{
+j < 10 ? fprintf(jStr, "0%d", j) : fprintf(jStr, "%d", j);
+printf("%d:%d\n", i, j);
+}
+}
 }
