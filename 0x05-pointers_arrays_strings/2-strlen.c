da6b8f68354f6a69d59a867b4ecea456fb6adb19
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-07-24 14:47:41 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include <string.h>
+
+/**
+ * _strlen - get lenght of passed string
+ * @s: pointer to the string
+ *
+ * Return: value of length
+ */
+int _strlen(char *s)
+{
+return (strlen(s));
+}
