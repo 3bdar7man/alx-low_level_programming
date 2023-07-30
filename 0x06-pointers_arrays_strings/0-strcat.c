2c0a2132d2f4e10eed132f8c1553093bb937a95d
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-07-30 13:51:41 -0700

    0-main.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+/**
+ * _strcat - like strcat
+ *@dest: the first string
+ *@src: the second string
+ *
+ * Return: the wanted string
+ */
+char *_strcat(char *dest, char *src)
+{
+return (strcat(dest, src));
+}
