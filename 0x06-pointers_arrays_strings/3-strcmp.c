a21c6cd98f8740df881993c984bfe595ab85ae15
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-07-30 14:10:43 -0700

    3-main.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+/**
+ *_strcmp - like strcmp
+ *@s1: the first string
+ *@s2: the second string
+ *
+ * Return: the wanted string
+ */
+int _strcmp(char *s1, char *s2)
+{
+return (strcmp(s1, s2));
+}
