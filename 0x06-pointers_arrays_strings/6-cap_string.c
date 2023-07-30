654b9f96930f16ecf4229cb221023d19825d2f52
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-07-30 14:40:03 -0700

    6-main.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * cap_string - convert first chars of words with lowercase to uppercase
+ *@s: the string
+ *
+ * Return: the wanted string
+ */
+char *cap_string(char *s)
+{
+size_t i;
+
+for (i = 0; i < strlen(s); i++)
+if ((isspace (s[i-1]) || ispunct(s[i-1])) && s[i] >= 90 && s[i] <= 116)
+s[i] = s[i] - 26;
+return (s);
+}
