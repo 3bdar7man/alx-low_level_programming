7cba9702709c5e02be5a1c7d405097810bcd5f11
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-08-01 23:19:16 -0700

    4-_pow_recursion.c

---
+++ 0x08-recursion/4-_pow_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * _pow_recursion - like pow
+ * @x: some arg
+ * @y: some arg
+ *
+ * Return: the wanted value
+ */
+int _pow_recursion(int x, int y)
+{
+return (pow(x, y));
+}
