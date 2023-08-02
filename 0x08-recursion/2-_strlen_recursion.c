5b01f20cef422bdc84a884792f725128e33d9d26
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-08-01 23:17:33 -0700

    2-_strlen_recursion.c

---
+++ 0x08-recursion/2-_strlen_recursion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen_recursion - like strlen
+ * @s: some arg
+ *
+ * Return: the wanted value
+ */
+int _strlen_recursion(char *s)
+{
+return (strlen(s));
+}
