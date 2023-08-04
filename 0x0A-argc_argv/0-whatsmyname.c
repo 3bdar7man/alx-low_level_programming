9bd92681436e80ea951bfcda236c5b48db01fed1
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-08-04 04:35:05 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - executed program
+ * @argc: some arg
+ * @argv: some arg
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+argc = argc;
+argv = argv;
+return (0);
+}
