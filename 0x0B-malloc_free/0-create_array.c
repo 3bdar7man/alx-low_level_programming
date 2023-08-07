9f60599c74c6e1508f5f2d645210c83c679ef303
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-08-07 11:19:00 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,12 +17,9 @@ char *create_array(unsigned int size, char c)
 	if (size < 1)
 		return ('\0');
 
-		str = malloc(size * sizeof(char));
+	str = malloc(size * sizeof(char));
 	if (str == NULL)
-	{
-		printf("Can't allocate %d bytes (after %d calls)\n", INT_MAX, i);
 		return (NULL);
-	}
 
 	for (i = 0; i < (int) size; i++)
 		str[i] = c;
