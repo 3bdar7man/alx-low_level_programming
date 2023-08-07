39e3d5e888588323f9053288ec21b663e1a70f12
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-08-07 11:09:37 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <limits.h>
 
-
 /**
  * create_array - some func
  * @size: some arg
@@ -12,20 +11,20 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	size_t i;
+	int i;
 	char *str;
 
 	if (size < 1)
 		return ('\0');
 
-	str = malloc(size * sizeof(char));
+		str = malloc(size * sizeof(char));
 	if (str == NULL)
 	{
 		printf("Can't allocate %d bytes (after %d calls)\n", INT_MAX, i);
-		return (1);
+		return (NULL);
 	}
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < (int) size; i++)
 		str[i] = c;
 	return (str);
 }
