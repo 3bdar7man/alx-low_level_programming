74efa338c47bf5564b5a1ca3126826280101042f
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-08-10 02:21:03 -0700

    DONE0-main.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,1 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
 
+
+/**
+ * malloc_checked - some func
+ * @b: some arg
+ *
+ * Return: the wanted value
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *Ptr = malloc(b);
+
+	if (Ptr == NULL)
+	{
+		printf("malloc error");
+		exit(98);
+	}
+
+	return (Ptr);
+}
