42fb7e7ebbcb67d4a5da34f42911301afb0db6db
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-08-10 01:06:33 -0700

    101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+
+/**
+ * main - executed program
+ * @argc: some arg
+ * @argv: some arg
+ *
+ * Return: the wanted value
+ */
+int main(int argc, char **argv)
+{
+	return (atoi(argv[1]) * atoi(argv[2]));
+}
