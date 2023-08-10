45c50c23afdb51e4e7da7223017a17c320ee8c9f
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-08-10 10:20:46 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+
+/**
+ * _calloc - some func
+ * @nmemb: some arg
+ * @size: some arg
+ *
+ * Return: the wanted value
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *Ptr = calloc(nmeb, size);
+
+	return (Ptr);
+}
