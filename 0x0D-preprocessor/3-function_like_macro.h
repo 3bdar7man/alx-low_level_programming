637fa338553de7fd7e6f97b1d9a3c116ca064720
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-08-14 06:45:16 -0700

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef _FUNCTION_LIKE_MACRO_H_
+#define _FUNCTION_LIKE_MACRO_H_
+
+#ifndef _STDIB_H_
+#define _STDIB_H_
+
+#include <stdlib.h>
+
+#endif /* _STDIB_H_ */
+
+#define ABS(x) (abs(x))
+
+#endif /* _FUNCTION_LIKE_MACRO_H_ */
