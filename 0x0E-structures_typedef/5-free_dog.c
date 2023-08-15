ffe4424678811a4800a69884d97c78035b172482
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-08-15 08:47:48 -0700

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,8 +1,5 @@
 #include "dog.h"
-<<<<<<< HEAD
 #include <stdlib.h>
-=======
->>>>>>> aef045e3562c868ba6a1d8ff9f36b03d60e008a2
 
 /**
  * free_dog - some func
@@ -10,10 +7,6 @@
  */
 void free_dog(dog_t *d)
 {
-<<<<<<< HEAD
 	if (d != (void  *)0)
 		free(d);
-=======
-	free(d);
->>>>>>> aef045e3562c868ba6a1d8ff9f36b03d60e008a2
 }
