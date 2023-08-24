77d7635ab91654db51188a0103171b6c0315f3a9
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-08-24 15:30:36 -0700

    2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node - some func
+ * @head: some arg
+ * @str: some arg
+ *
+ * Return: the wanted value
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *h = malloc(sizeof(list_t) * 1);
+if (h == (void *) 0)
+exit(1);
+
+if (head == (void *) 0)
+exit(1);
+
+if (*head == (void *) 0)
+exit(1);
+
+h->str = strdup(str);
+h->len = strlen(str);
+if ((*head)->next == (void *) 0) 
+{
+(*head)->next = h;
+h->next = (void *) 0;
+}
+else
+{
+h->next = (*head)->next;
+(*head)->next = h;
+h->next = (void *) 0;
+}
+
+return (h);
+}
