89e2c0b1e5eaa0a4c853555738ff875ff730a836
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-08-24 14:35:12 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * print_list - some func
+ * @h: some arg
+ *
+ * Return: the wanted value
+ */
+size_t print_list(const list_t *h)
+{
+size_t n = 0;
+char *str;
+
+if (h == (void *) 0)
+{
+printf("[0] (nil)");
+return (0);
+}
+
+while (h)
+{
+n++;
+str = strdup(h->str);
+printf("[%d] %c%s%c", h->len, '"', str, '"');
+h = h->next;
+}
+
+return (n);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define _LISTS_H_
 
 #include "list_t.h"
+#include <stddef.h>
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
