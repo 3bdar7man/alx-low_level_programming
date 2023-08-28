8bb343f9b96d912bd7fb22e5721bbec1c2632626
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-08-28 05:17:24 -0700

    listint_t.h

---
+++ 0x13-more_singly_linked_lists/listint_t.h
@@ -0,0 +1,18 @@
+#ifndef _LISTINT_T_H_
+#define _LISTINT_T_H_
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+#endif  /* _LISTINT_T_H_ */
