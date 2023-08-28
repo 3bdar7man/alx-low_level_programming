9a862b0d200571deca00ad2eb0f4c92af95bda18
Abdarhman Qassem <elfarselmghool@gmail.com>
2023-08-28 06:04:23 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,28 +1,22 @@
 #include "lists.h" /* with #include <stddef.h> */
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * print_listint - some func
+ * listint_len - some func
  * @h: some arg
  *
  * Return: the wanted value
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 size_t n = 0;
 
 if (h == (void *) 0)
-{
-printf("0");
 return (0);
-}
 
 while (h)
 {
 n++;
-printf("%d\n", h->n);
 h = h->next;
 }
 
